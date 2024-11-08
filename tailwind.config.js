ed261a7afc70138579934790738c95dfc6a9a991
ThalissonBr <breno.thalisson09@gmail.com>
2024-11-08 01:29:48 -0300

    finalizando ingormações venus

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
         'text-shadow': '0px 4px 4px rgba(0, 0, 0, 0.25)',
       },
       backgroundImage: {
-        'custom-bg': "url('/assets/image.png')",
+        'custom-bg': "url('/assets/background.png')",
       },
       colors: {
         'color-bg': '#070724',
-      },
+        'color-hover': '#EDA249',
+            },
       boxShadow: {
         'custom-shadow': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)', 
       },
